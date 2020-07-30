d0b796c93eb45e773586adfaf1fc1ed38bcedf3f
Ricthofen <jazzplato@gmail.com>
2020-07-30 02:41:15 -0400

    add sql to init the db upon starting up

--- system/database/sql/init.sql
+++ system/database/sql/init.sql
@@ -1,5 +1,6 @@
 USE mysql;
-CREATE DATABASE IF NOT EXISTS KishoCinema;
+SET NAMES UTF8;
+CREATE DATABASE IF NOT EXISTS KishoCinema CHARACTER SET utf8 COLLATE utf8_bin;
 CREATE USER IF NOT EXISTS 'kisho'@'localhost' IDENTIFIED BY 'KishoKurokawa';
 GRANT ALL ON KishoCinema.* to 'kisho'@'localhost';
 FLUSH PRIVILEGES;
